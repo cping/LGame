c12dc07eca99f1b47d0bb1d0290e8c9e7ad86761
cping <longwind2012@hotmail.com>
2013-01-19 09:14:13 +0800

    submit javascript tool
    submit javascript tool

---
+++ JavaScript/js(min)/tools/loon-build(js compress).bat
@@ -0,0 +1,1 @@
+cmd /k java -jar compiler.jar --js_output_file ../loon.min.js --js ../src/loon-core.js
---
+++ JavaScript/js(min)/tools/loon-build(js compress).sh
@@ -0,0 +1,1 @@
+java -jar compiler.jar --js_output_file ../loon.min.js --js ../src/loon-core.js
