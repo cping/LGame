38d3641ca108630739d35f78592049021a4a2463
cping <longwind2012@hotmail.com>
2017-01-11 17:54:23 +0800

    update
    update

--- Java/Loon-Neo/src/loon/action/page/BasePage.java
+++ Java/Loon-Neo/src/loon/action/page/BasePage.java
@@ -3,18 +3,8 @@ package loon.action.page;
 import loon.Screen;
 
 public abstract class BasePage {
-	
-	public boolean pageIn = false;
 
 	public abstract void onTransform(Screen screen, float position);
-
-	public boolean hideOffscreenPages() {
-		return true;
-	}
-
-	public boolean isPagingEnabled() {
-		return false;
-	}
 
 	public void resetTransform(Screen screen) {
 		screen.setRotation(0);
